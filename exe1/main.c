33260c037d7718d81e970206dc45ceb16c0d9118
Gabriel <gabrielog@ali.insper.edu.br>
2025-04-13 20:29:30 -0300

    ex1 tentativa 2

--- exe1/main.c
+++ exe1/main.c
@@ -31,7 +31,7 @@ void i2c_task(void *p) {
     // TODO
     // Configure o acc para operar em 4G
     buf_write[0] = MPUREG_ACCEL_CONFIG;
-    buf_write[1] = 0x08;
+    buf_write[1] = 1 << 3;
     i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, buf_write, 2, false);
     
 
